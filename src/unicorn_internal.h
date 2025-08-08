c6988980907cb53e86bc0ebf3d35dcdf1b9c6bde
Julian Regalado <jregalado@bicu.dev>
2025-08-08 15:13:50 +0200

    this commit is why you test :)

--- src/unicorn_internal.h
+++ src/unicorn_internal.h
@@ -299,7 +299,7 @@ typedef struct _covstats_t {
                  "n_entropy\t"\
                  "n_gini\t"\
                  "tad80\n"
-#define TIDSTATSTR "#taxid\tname\tnum_accessions\ttotal_length"\
+#define TIDSTATSTR "#taxid\tname\tnum_accessions\ttotal_length\t"\
                 	 "n_alns\tn_reads\tm_readl\tstd_readl\t"\
                 	 "median_readl\tmode_readl\treadl_min\treadl_max\t"\
                 	 "mean_alnnm\tmean_alnani\tstdev_alnani\tmedian_alnani\t"\
@@ -307,7 +307,7 @@ typedef struct _covstats_t {
 									 "breath_ratio\tmean_covcovered\tstdev_covoncovered\tevenness_cov\t"\
                 	 "site_density\tentropy\tgini\tnorm_entropy\t"\
                 	 "norm_gini\ttad80\n"
-#define TIDFMTSTR "#%u\t%s\t%u\t%"PRIu64"\t"\
+#define TIDFMTSTR "%u\t%s\t%u\t%"PRIu64"\t"\
 									"%u\t%u\t%u\t%f\t"\
 									"%u\t%u\t%u\t%u\t"\
 									"%f\t%f\t%f\t%u\t"\
